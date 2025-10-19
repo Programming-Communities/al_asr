307aeb363cab24add0d347dfba276f9bb7f5baba
Aamir Shahzadah <aamirshahzadah@gmail.com>
2025-10-19 08:49:50 +0500

    add: tsconfig.json for alias support

--- Components/Header.jsx
+++ Components/Header.jsx
@@ -1,5 +1,6 @@
 'use client'
 import { assets } from "../Assets/assets";
+
 import Image from "next/image";
 import React, { useState, useEffect } from "react";
 
--- next.config.js
+++ next.config.js
@@ -1,8 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['admin-al-asr.centers.pk', 'al-asr.centers.pk'],
+    domains: ['admin-al-asr.centers.pk'],
   },
-};
+  // Add this for alias support
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': __dirname,
+    }
+    return config
+  },
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
